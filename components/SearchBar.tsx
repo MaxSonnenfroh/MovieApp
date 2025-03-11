bf317f7073dfc3b3e3d01010bd8676ed644067c0
DerTeddy <max.sonnenfroh@freenet.de>
2025-03-11 23:16:44 +0100

    Implement search functionality in the Search component; add SearchBar and integrate movie fetching with debounced input

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,25 +1,31 @@
 import { icons } from '@/constants/icons';
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { View, Text, Image, TextInput } from 'react-native';
 
 interface SearchBarProps {
     onPress?: () => void;
     placeholder: string;
+    value?: string;
+    onChangeText?: (text: string) => void;
+    autoFocus?: boolean;
 }
 
-const SearchBar = ({onPress, placeholder}: SearchBarProps) => {
+const SearchBar = ({ onPress, placeholder, value, onChangeText, autoFocus = false }: SearchBarProps) => {
     return (
         <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
             <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#abbff"/>
             <TextInput
                 onPress={onPress}
                 placeholder={placeholder}
-                value=''
-                onChangeText={() => {}}
+                value={value}
+                onChangeText={onChangeText}
                 placeholderTextColor={'#a8b5bd'}
+                className='flex-1 text-white ml-2'
+                autoFocus={autoFocus}
+                clearTextOnFocus={true}
             />
         </View>
     );
-}
+};
 
 export default SearchBar;
